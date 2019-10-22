d1f3f16bbe870ca03c51a4e1fdf14cc4a619618c
chengaoyuan <chen.gaoyuan@qq.com>
2019-10-22 15:48:58 +0800

    fixbug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function shuffle<T>(data: T[]) {
     const length = data.length;
-    for (let posA = 0; posA < length; posA--) {
+    for (let posA = data.length - 1; posA >= 0; posA--) {
         const posB = Math.floor(Math.random() * length);
         const temp = data[posA];
         data[posA] = data[posB];
